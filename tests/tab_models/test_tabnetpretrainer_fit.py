bc945ff0bb0ee809e0f769ce70103ab87887375f
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2025-01-01 21:11:59 +0200

    Refactor and enhance TabNetPretrainer test cases.
    Removed unused fixtures and updated parameterized tests to include new cases. Added save/load model functionality and prediction verification to improve test coverage.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- tests/tab_models/test_tabnetpretrainer_fit.py
+++ tests/tab_models/test_tabnetpretrainer_fit.py
@@ -2,17 +2,7 @@ import pytest
 import numpy as np
 from pytorch_tabnet.pretraining import TabNetPretrainer
 
-# @pytest.fixture
-# def X_train():
-#     return np.random.rand(100, 10)
-#
-# @pytest.fixture
-# def X_valid():
-#     return np.random.rand(50, 10)
-#
-# @pytest.fixture
-# def unsupervised_model():
-#     return TabNetPretrainer()
+
 @pytest.mark.parametrize(
     "model_params, fit_params, X_train, X_valid",
     [
@@ -23,10 +13,16 @@ from pytorch_tabnet.pretraining import TabNetPretrainer
         np.random.rand(50, 10)
         ),
         (
-        dict(cat_idxs=[0, 1, 2, 3, 4], cat_dims=[5, 5, 5, 5, 5], ),
-        dict(pretraining_ratio=0.8, max_epochs=1, batch_size=32, virtual_batch_size=32),
+                dict(cat_idxs=[0, 1, 2, 3, 4], cat_dims=[5, 5, 5, 5, 5], ),
+                dict(pretraining_ratio=0.8, max_epochs=1, batch_size=32, virtual_batch_size=32),
+                np.random.rand(100, 10),
+                np.random.rand(50, 10)
+        ),
+        (
+        dict(cat_idxs=[0, 1, 2, 3, 4], cat_dims=[5, 5, 5, 5, 5],cat_emb_dim=5),
+        dict(pretraining_ratio=0.8, max_epochs=1, batch_size=16, virtual_batch_size=32),
         np.random.rand(100, 10),
-        np.random.rand(50, 10)
+        np.random.rand(200, 10)
 )
 
 ,
@@ -45,3 +41,7 @@ def test_pretrainer_fit(model_params,fit_params, X_train, X_valid):
     assert hasattr(unsupervised_model, 'network')
     assert unsupervised_model.network.pretraining_ratio == 0.8
     assert unsupervised_model.history.epoch_metrics
+    unsupervised_model.save_model("test_model")
+    unsupervised_model.load_model("test_model.zip")
+    pred,_ = unsupervised_model.predict(X_valid)
+    assert pred.shape[0] == X_valid.shape[0]
