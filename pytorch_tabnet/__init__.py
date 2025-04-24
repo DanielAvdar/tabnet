218457304bcf512d0679ff9c5a160d27bfe5a1d7
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2025-04-24 11:28:14 +0300

    Add MultiTabNetRegressor import to module init
    The `MultiTabNetRegressor` class was added to the module's `__init__.py` for proper exposure and accessibility. This ensures consistent imports and aligns with other classes defined in `tab_model`.
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- pytorch_tabnet/__init__.py
+++ pytorch_tabnet/__init__.py
@@ -2,9 +2,9 @@
 
 from importlib.metadata import version
 
-from .metrics import Metric as Metric
 from .multitask import TabNetMultiTaskClassifier as TabNetMultiTaskClassifier
 from .pretraining import TabNetPretrainer as TabNetPretrainer
+from .tab_model import MultiTabNetRegressor as MultiTabNetRegressor
 from .tab_model import TabNetClassifier as TabNetClassifier
 from .tab_model import TabNetRegressor as TabNetRegressor
 
