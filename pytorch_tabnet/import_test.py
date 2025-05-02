5e549b05401caa59268fbb343935ac1859b6ab64
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2025-05-02 13:40:28 +0300

    Add import tests for TabNet models
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

---
+++ pytorch_tabnet/import_test.py
@@ -0,0 +1,5 @@
+def test_imports():
+    from pytorch_tabnet.tab_model import TabNetClassifier, TabNetRegressor # noqa
+    from pytorch_tabnet.multitask import TabNetMultiTaskClassifier # noqa
+    from pytorch_tabnet.pretraining import TabNetPretrainer # noqa
+    assert True
