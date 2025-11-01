320321155eea5182a588bcc098e32cedd875add2
Daniel Avdar <66269169+DanielAvdar@users.noreply.github.com>
2025-11-01 17:50:42 +0200

    Fix group_attention_matrix initialization logic
    Refactor group_attention_matrix initialization and registration.
    
    Signed-off-by: Daniel Avdar <66269169+DanielAvdar@users.noreply.github.com>

--- pytorch_tabnet/tab_network/tabnet_encoder.py
+++ pytorch_tabnet/tab_network/tabnet_encoder.py
@@ -73,17 +73,14 @@ class TabNetEncoder(torch.nn.Module):
         self.virtual_batch_size = virtual_batch_size
         self.mask_type = mask_type
         self.initial_bn = BatchNorm1d(self.input_dim, momentum=0.01)
+        self.group_attention_matrix = group_attention_matrix
 
-        if group_attention_matrix is None:
+        if self.group_attention_matrix is None:
             # no groups
-            group_attention_matrix = torch.eye(self.input_dim)
+            self.group_attention_matrix = torch.eye(self.input_dim)
             self.attention_dim = self.input_dim
         else:
-            self.attention_dim = group_attention_matrix.shape[0]
-
-        # Register as buffer to ensure it moves with the model when .to(device) is called
-        self.register_buffer("group_attention_matrix", group_attention_matrix)
-
+            self.attention_dim = self.group_attention_matrix.shape[0]
         if self.n_shared > 0:
             shared_feat_transform = torch.nn.ModuleList()
             for i in range(self.n_shared):
